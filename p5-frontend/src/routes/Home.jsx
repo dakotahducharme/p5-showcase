757f555a052e15f60103e65a27bff06a888bfbc2
Mouse <dakotahbducharme@gmail.com>
2022-08-11 15:33:10 -0700

    updating homepage css

--- p5-frontend/src/routes/Home.jsx
+++ p5-frontend/src/routes/Home.jsx
@@ -5,7 +5,7 @@ function Home () {
     return(
         <div>
             <App></App>
-            <div>
+            <div id="homepage">
                 p5.js is a javascript library for creative coding. In this project,
                 I demonstrate 4 examples of how to use the p5 library in a react application.
             </div>
